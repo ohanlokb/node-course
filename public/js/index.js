992dd22f25223048db16a02a029dc27d57ef6c58
Kevin <ohanlokb@hotmail.com>
2019-01-16 16:26:33 +0000

    Updated

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function (){
     console.log('Connected to server.');
-    
-    socket.emit('createMessage', {
-        from: 'kev@test.com',
-        text: 'Hi'
-    });
 });
 
 socket.on('disconnect', function (){
