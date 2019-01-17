61c11b0d3ab1873c0822a68481bc27474eb5d442
BuildTools <unconfigured@null.spigotmc.org>
2019-01-16 21:07:01 -0500

    added styles

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,12 @@ socket.on('newLocationMessage', function (message){
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+    let messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -46,12 +47,16 @@ locationButton.on('click', function (e) {
         return alert('Geolocation not supported by you browser.');
     }
 
+    locationButton.attr('disabled','disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location.')
     });
 });
